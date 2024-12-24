faa83e33b45da62a181e973c1b7b3ee54408c4c0
sfomuseumbot <sfomuseumbot@localhost>
2024-12-23 16:04:49 -0800

    trying to figure out how to define scripts

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='sfomuseum.api',
     packages=find_namespace_packages(include=["sfomuseum.*"]),
     namespace_packages=["sfomuseum"],
     version='0.0.1',
-    packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=[],
     description='Python package for using the SFO Museum API',
+    scripts=["scripts/sfomuseum-api"],
     author='SFO Museum',
     author_email='info@sfomuseum.org',
     url='https://github.com/sfomuseum/py-sfomuseum-api',
